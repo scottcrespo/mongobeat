4bc5079237503cf034ad3e9c577b83536305656f
Scott Crespo <screspo@highcornergroup.com>
2016-11-26 19:13:02 -0500

    db stats uses structs package to convert database response to map[string]interface. which then gets converted added to common.MapStr

--- beater/db_stats.go
+++ beater/db_stats.go
@@ -1,27 +1,30 @@
 package beater
 
 import (
-	"encoding/json"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/elastic/beats/libbeat/beat"
 	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/libbeat/logp"
+
+	"github.com/fatih/structs"
 )
 
+// DbStats represents the fields returned from a call to db.stats() in Mongo
 type DbStats struct {
-	Db          string `bson:"db" json:"db"`
-	Collections int64  `bson:"collections" json:"collections"`
-	Objects     int64  `bson:"objects" json:"objects"`
+	Db          string `bson:"db"            json:"db"`
+	Collections int64  `bson:"collections"   json:"collections"`
+	Objects     int64  `bson:"objects"       json:"objects"`
 	AvgObjSize  int64  `bson:"avgObjectSize" json:"avgObjSize"`
-	DataSize    int64  `bson:"dataSize" json:"dataSize"`
-	StorageSize int64  `bson:"storageSize" json:"storageSize"`
-	NumExtents  int64  `bson:"numExtents" json:"numExtents"`
-	Indexes     int64  `bson:"indexes" json:"indexes"`
-	IndexSize   int64  `bson:"indexSize" json:"indexSize"`
-	FileSize    int64  `bson:"fileSize" json:"fileSize"`
-	Ok          int    `bson:"ok" json:"ok"`
+	DataSize    int64  `bson:"dataSize"      json:"dataSize"`
+	StorageSize int64  `bson:"storageSize"   json:"storageSize"`
+	NumExtents  int64  `bson:"numExtents"    json:"numExtents"`
+	Indexes     int64  `bson:"indexes"       json:"indexes"`
+	IndexSize   int64  `bson:"indexSize"     json:"indexSize"`
+	FileSize    int64  `bson:"fileSize"      json:"fileSize"`
+	Ok          int    `bson:"ok"            json:"ok"`
 }
 
 // publishDbStats calls db.stats() command for each database in the mongo instance and
@@ -38,17 +41,22 @@ func (bt *Mongobeat) publishDbStats(b *beat.Beat) {
 
 		results := DbStats{}
 
-		db.Run("dbStats", &results)
+		err := db.Run("dbStats", &results)
+		if err != nil {
+			logp.Err("Failed to retrieve stats for db %s", dbName)
+			continue
+		}
 
-		resultsJson, err := json.Marshal(results)
-		fmt.Printf("%v", err)
-		fmt.Printf("%s", resultsJson)
+		resultsMap := structs.Map(results)
 
 		event := common.MapStr{
 			"@timestamp": common.Time(time.Now()),
 			"type":       fmt.Sprintf("%s.dbStats", b.Name),
 		}
 
+		event.Update(resultsMap)
+
+		log.Fatal(event)
 		bt.client.PublishEvent(event)
 		logp.Info("Event sent")
 	}
