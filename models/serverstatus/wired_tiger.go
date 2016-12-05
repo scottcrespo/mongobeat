54bef3367148d1641b1b34baea925e1ee5e40f41
Scott Crespo <screspo@highcornergroup.com>
2016-12-04 23:54:31 -0500

    added struct tags

--- models/serverstatus/wired_tiger.go
+++ models/serverstatus/wired_tiger.go
@@ -2,9 +2,9 @@ package serverstatus
 
 // WiredTiger ...
 type WiredTiger struct {
-	URI string `bson:"uri" json:"uri"`
-	LSM `bson:"LSM" json:"lsm"`
-	Async
+	URI   string `bson:"uri" json:"uri"`
+	LSM   `bson:"LSM" json:"lsm"`
+	Async `bson:"async" json:"async"`
 	BlockManager
 	Cache
 	Connection
@@ -34,10 +34,10 @@ type LSM struct {
 
 // Async ...
 type Async struct {
-	NumberOfAllocationStateRaces              uint `bson:"number of allocation state races" json:"number_of_allocation_state_races"`
-	NumberOfOperationSlotsViewedForAllocation uint `bson:"number of operation slots viewed for allocation" json:"number_of_operation_slots_viewed_for_allocation"`
-	CurrentWorkQueueLength                    uint `bson:"current work queue length" json:"current_work_queue_length"`
-	NumberOfFlushCalls                        uint `bson:"number of flush calls" json:"number_of_flush_calls"`
+	NumberOfAllocationStateRaces              uint `bson:"number of allocation state races" json:"number_of_allocation_state_races,omitempty"`
+	NumberOfOperationSlotsViewedForAllocation uint `bson:"number of operation slots viewed for allocation" json:"number_of_operation_slots_viewed_for_allocation,omitempty"`
+	CurrentWorkQueueLength                    uint `bson:"current work queue length" json:"current_work_queue_length,omitempty"`
+	NumberOfFlushCalls                        uint `bson:"number of flush calls" json:"number_of_flush_calls,omitempty"`
 	NumberOfTimesOperationAllocationFailed    uint `bson:"number of times operation allocation failed" json:"number_of_times_operation_allocation_failed"`
 	MaximumWorkQueueLength                    uint `bson:"maximum work queue length" json:"maximum_work_queue_length"`
 	NumberOfTimesWorkerFoundNoWork            uint `bson:"number of times worker found no work" json:"number_of_times_worker_found_no_work"`
