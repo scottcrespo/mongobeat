0bf5717ab39ad20fc73d39898bb7686ba0fc8d8e
Scott Crespo <screspo@highcornergroup.com>
2016-12-07 21:08:19 -0500

    ensuring compatibility between versions of mongo 3.x

--- models/serverstatus/storage_engine.go
+++ models/serverstatus/storage_engine.go
@@ -2,7 +2,9 @@ package serverstatus
 
 // StorageEngine is the 'storageEngine' key of serverStatus output
 type StorageEngine struct {
-	Name                   string `bson:"name"                       json:"name"`
-	SupportsCommittedReads bool   `bson:"supportsCommittedReads"     json:"supports_committed_reads"`
-	Persistent             bool   `bson:"persistent"                 json:"persistent"`
+	Name string `bson:"name"                       json:"name"`
+	// New in 3.2
+	SupportsCommittedReads bool `bson:"supportsCommittedReads"     json:"supports_committed_reads"`
+	// New in 3.2.6
+	Persistent bool `bson:"persistent"                 json:"persistent"`
 }
