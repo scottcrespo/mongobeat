f80237b6ef244322c3c32757c2eeb442ed14f2f4
Scott Crespo <screspo@highcornergroup.com>
2016-12-08 21:22:16 -0500

    new concurrency pattern that spawns threads for each node and metric

--- beater/db_stats.go
+++ beater/db_stats.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"time"
 
+	"gopkg.in/mgo.v2"
+
 	"github.com/elastic/beats/libbeat/beat"
 	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/libbeat/logp"
@@ -11,47 +13,59 @@ import (
 	"github.com/scottcrespo/mongobeat/models/dbstats"
 )
 
-// getDbStats calls db.stats() command and appends to a common.MapStr, with root key as
-// "dbStats"
-func (bt *Mongobeat) getDbStats(b *beat.Beat) {
+// monitorDbStats calls db.stats() command for each database on the instance. This function runs
+// in an event loop and sends reports according to the configured period
+func (bt *Mongobeat) monitorDbStats(b *beat.Beat, node *mgo.Session, ticker *time.Ticker) {
 
-	dbs, err := bt.getDatabases()
-	if err != nil {
-		logp.Err("DbStats: Failed to retrieve list of databases from mongo instance")
-		return
-	}
+	for {
+		select {
 
-	// store common event info here
-	eventTime := common.Time(time.Now())
-	eventType := fmt.Sprintf("%s.db_stats", b.Name)
+		case <-bt.done:
+			return
 
-	for _, dbName := range dbs {
-		db := bt.mongoConn.DB(dbName)
+		case <-ticker.C:
+		}
 
-		results := dbstats.DbStats{}
-
-		err := db.Run("dbStats", &results)
+		// Get a list of databases stored on the node
+		dbs, err := bt.getDatabases(node)
 		if err != nil {
-			logp.Err("Failed to retrieve stats for db %s", dbName)
-			continue
+			logp.Err("DbStats: Failed to retrieve list of databases from mongo instance")
+			return
 		}
 
-		// instantiate event
-		event := common.MapStr{
-			"@timestamp": eventTime,
-			"type":       eventType,
-			"db_stats":   results,
-		}
+		// store common event info here
+		eventTime := common.Time(time.Now())
+		eventType := fmt.Sprintf("%s.db_stats", b.Name)
 
-		// fire
-		bt.client.PublishEvent(event)
-		logp.Info("dbStats Event sent")
+		// for each database, call db.stats() and publish the event
+		for _, dbName := range dbs {
+			db := node.DB(dbName)
+
+			results := dbstats.DbStats{}
+
+			err := db.Run("dbStats", &results)
+			if err != nil {
+				logp.Err("Failed to retrieve stats for db %s", dbName)
+				continue
+			}
+
+			// instantiate event
+			event := common.MapStr{
+				"@timestamp": eventTime,
+				"type":       eventType,
+				"db_stats":   results,
+			}
+
+			// fire
+			bt.client.PublishEvent(event)
+		}
+		logp.Info("db_stats Event sent")
 	}
 }
 
 // getDatabases retrieves current list of databases in the Mongo instance
-func (bt *Mongobeat) getDatabases() ([]string, error) {
-	dbs, err := bt.mongoConn.DatabaseNames()
+func (bt *Mongobeat) getDatabases(node *mgo.Session) ([]string, error) {
+	dbs, err := node.DatabaseNames()
 	if err != nil {
 		logp.Err("Error retrieving mongo database names from Mongo instance")
 		return []string{}, err
